738455fce98359a523ff13ef2260639f2a4bd63f
pavleerg <pavleergovic@gmail.com>
2022-09-01 14:48:10 +0200

    changed some django models

--- bruc/models.py
+++ bruc/models.py
@@ -13,7 +13,8 @@ class Guests(models.Model):
     tag=models.CharField(max_length=50, default='', blank=True)
     bought=models.CharField(max_length=50, default='0', blank=True)
     entered=models.CharField(max_length=50, default='', blank=True)
-    deleted=models.CharField(max_length=50, default='', blank=True)
+    confCode=models.CharField(max_length=50, default='', blank=True)
+
 
 class Users(models.Model):
     id=models.CharField(max_length=50, primary_key=True, default='', blank=True)
@@ -32,7 +33,6 @@ class Lineup(models.Model):
     id=models.CharField(max_length=50, primary_key=True, default=0, blank=False)
     slug=models.CharField(max_length=200, default='', blank=True)
     name=models.CharField(max_length=49, default='', blank=True)
-    url=models.CharField(max_length=400, default='0', blank=True)
     image = models.ImageField(upload_to='uploads/lineup', blank=True, null=True)
 
     '''def get_image(self):
@@ -54,6 +54,8 @@ class Sponsors(models.Model):
     name=models.CharField(max_length=49, default='', blank=True)
     url=models.CharField(max_length=400, default='0', blank=True)
     image = models.ImageField(upload_to='uploads/sponsors', blank=True, null=True)
+    email=models.CharField(max_length=49, default='', blank=True)
+    guestCap=models.CharField(max_length=49, default='', blank=True)
 
     '''def get_image(self):
         if self.image:
--- bruc/serializer.py
+++ bruc/serializer.py
@@ -5,7 +5,7 @@ from .models import Guests, Tags, Users, Lineup, Sponsors
 class GuestsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Guests
-        fields = ["id","name","surname","jmbag","email","tag","bought","entered","deleted"]
+        fields = ["id","name","surname","jmbag","email","tag","bought","entered","confCode"]
 
 class TagsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -20,12 +20,12 @@ class UsersSerializer(serializers.HyperlinkedModelSerializer):
 class LineupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Lineup
-        fields = ["id","slug","name","url","image"]
+        fields = ["id","slug","name","image"]
 
 class SponsorsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Sponsors
-        fields = ["id","slug","name","url","image"]
+        fields = ["id","slug","name","url","image","email","guestCap"]
 
 class DynamicSearchFilter(filters.SearchFilter):
     def get_search_fields(self, view, request):
