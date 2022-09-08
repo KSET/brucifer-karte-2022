0f483c14250998e927b22b5fe7a24bcbdab7e9b1
Marin Capan <marin.capan@gmail.com>
2022-09-09 00:14:28 +0200

    fixed gitignore

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,4 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser
-python manage.py loaddata initial_MK2ZK_data.json
 python manage.py runserver 0.0.0.0:8000
