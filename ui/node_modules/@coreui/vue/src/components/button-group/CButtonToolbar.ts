a69550da7c8e206edb4b304bf3b4e639836e295e
Marin Capan <marin.capan@gmail.com>
2022-09-09 00:12:39 +0200

    fixed dockerfile

--- ui/node_modules/@coreui/vue/src/components/button-group/CButtonToolbar.ts
+++
@@ -1,9 +0,0 @@
-import { defineComponent, h } from 'vue'
-
-const CButtonToolbar = defineComponent({
-  name: 'CButtonToolbar',
-  setup(_, { slots }) {
-    return () => h('div', { class: 'btn-toolbar' }, slots.default && slots.default())
-  },
-})
-export { CButtonToolbar }
--- ui/node_modules/@coreui/vue/src/directives/index.ts
+++
@@ -1,5 +0,0 @@
-import vcplaceholder from './v-c-placeholder'
-import vcpopover from './v-c-popover'
-import vctooltip from './v-c-tooltip'
-
-export { vcplaceholder, vcpopover, vctooltip,  }
