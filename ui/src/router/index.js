cf540c4dd1a980143fa9213a5a7192a2b64cdbb9
pavleerg <pavleergovic@gmail.com>
2022-09-25 11:55:51 +0200

    alligned elements in files

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,210 +1,239 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 /* Buckarte page Views */
-import Guests from '../views/BruckarteViews/GuestsView.vue'
-import EntryView from '../views/BruckarteViews/EntryView.vue'
-import Login from '../views/BruckarteViews/LoginView.vue'
-import Logout from '../views/BruckarteViews/LogoutView.vue'
-import PageNotFound from '../views/BruckarteViews/PageNotFound.vue'
-
-
+import Guests from "../views/BruckarteViews/GuestsView.vue";
+import EntryView from "../views/BruckarteViews/EntryView.vue";
+import Login from "../views/BruckarteViews/LoginView.vue";
+import Logout from "../views/BruckarteViews/LogoutView.vue";
+import PageNotFound from "../views/BruckarteViews/PageNotFound.vue";
 
 /* Admin Views */
-import Tags from '../views/AdminViews/TagsView.vue'
-import Users from '../views/AdminViews/UsersView.vue'
-import Privileges from '../views/AdminViews/PrivilegesView.vue'
-import Import from '../views/AdminViews/ImportView.vue'
-import LineupList from '../views/AdminViews/LineupListView.vue'
-import SponsorsList from '../views/AdminViews/SponsorsListView.vue'
-import AdminPanel from '../views/AdminViews/AdminPanelView.vue'
-import LineupAdd from '../views/AdminViews/LineupAddView.vue'
-import SponsorsAdd from '../views/AdminViews/SponsorsAddView.vue'
-import GuestAdd from '../views/AdminViews/GuestAddView.vue'
-import BandKontakt from '../views/AdminViews/BandKontaktView.vue'
-
+import Tags from "../views/AdminViews/TagsView.vue";
+import Users from "../views/AdminViews/UsersView.vue";
+import Privileges from "../views/AdminViews/PrivilegesView.vue";
+import Import from "../views/AdminViews/ImportView.vue";
+import LineupList from "../views/AdminViews/LineupListView.vue";
+import SponsorsList from "../views/AdminViews/SponsorsListView.vue";
+import AdminPanel from "../views/AdminViews/AdminPanelView.vue";
+import LineupAdd from "../views/AdminViews/LineupAddView.vue";
+import SponsorsAdd from "../views/AdminViews/SponsorsAddView.vue";
+import GuestAdd from "../views/AdminViews/GuestAddView.vue";
+import BandKontakt from "../views/AdminViews/BandKontaktView.vue";
 
 /* BruciWeb Views */
-import Bwlineup from '../views/BruciWebViews/BWlineupView.vue'
-import BWsponsors from '../views/BruciWebViews/BWsponsorsView.vue'
-import Kontakt from '../views/BruciWebViews/KontaktView.vue'
-import Ulaznice from '../views/BruciWebViews/UlazniceView.vue'
-import Pravila from '../views/BruciWebViews/PravilaView.vue'
-import Naslovnica from '../views/BruciWebViews/NaslovnicaView.vue'
-import BWPageNotFound from '../views/BruciWebViews/BWPageNotFound.vue'
+import Bwlineup from "../views/BruciWebViews/BWlineupView.vue";
+import BWsponsors from "../views/BruciWebViews/BWsponsorsView.vue";
+import Kontakt from "../views/BruciWebViews/KontaktView.vue";
+import Ulaznice from "../views/BruciWebViews/UlazniceView.vue";
+import Pravila from "../views/BruciWebViews/PravilaView.vue";
+import Naslovnica from "../views/BruciWebViews/NaslovnicaView.vue";
+import BWPageNotFound from "../views/BruciWebViews/BWPageNotFound.vue";
 
-
-import store from '@/store/index.js';
-
-
+import store from "@/store/index.js";
 
 const routes = [
   {
-    path: '/bruckarte/',
-    name: 'home',
+    path: "/bruckarte/",
+    name: "home",
   },
 
   /* Bruckarte Views setup */
   {
-    path: '/bruckarte/tags',
-    name: 'tags',
-    component : Tags,
+    path: "/bruckarte/tags",
+    name: "tags",
+    component: Tags,
   },
   {
-    path: '/bruckarte/guests',
-    name: 'guests',
-    component : Guests
+    path: "/bruckarte/guests",
+    name: "guests",
+    component: Guests,
   },
   {
-    path: '/bruckarte/entry',
-    name: 'guest_tag',
-    component : EntryView
+    path: "/bruckarte/entry",
+    name: "guest_tag",
+    component: EntryView,
   },
   {
-    path: '/bruckarte/users',
-    name: 'users',
-    component : Users
+    path: "/bruckarte/users",
+    name: "users",
+    component: Users,
   },
   {
-    path: '/bruckarte/privileges',
-    name: 'privileges',
-    component : Privileges
+    path: "/bruckarte/privileges",
+    name: "privileges",
+    component: Privileges,
   },
   {
-    path: '/bruckarte/import',
-    name: 'import',
-    component : Import
-  }
-  ,
-  {
-    path: '/bruckarte/login',
-    name: 'login',
-    component : Login
+    path: "/bruckarte/import",
+    name: "import",
+    component: Import,
   },
   {
-    path: '/bruckarte/logout',
-    name: 'logout',
-    component : Logout
+    path: "/bruckarte/login",
+    name: "login",
+    component: Login,
   },
   {
-    path: '/bruckarte/lineup-list',
-    name: 'lineup',
-    component : LineupList
+    path: "/bruckarte/logout",
+    name: "logout",
+    component: Logout,
   },
   {
-    path: '/bruckarte/sponsors/:slug',
-    name: 'sponsors',
-    component : SponsorsList
+    path: "/bruckarte/lineup-list",
+    name: "lineup",
+    component: LineupList,
   },
   {
-    path: '/bruckarte/sponsors-list',
-    name: 'sponsors',
-    component : SponsorsList
+    path: "/bruckarte/sponsors/:slug",
+    name: "sponsors",
+    component: SponsorsList,
   },
   {
-    path: '/bruckarte/guests-add',
-    name: 'guest-add',
-    component : GuestAdd
+    path: "/bruckarte/sponsors-list",
+    name: "sponsors",
+    component: SponsorsList,
   },
   {
-    path: '/bruckarte/admin-panel',
-    name: 'admin-panel',
-    component : AdminPanel
+    path: "/bruckarte/guests-add",
+    name: "guest-add",
+    component: GuestAdd,
   },
   {
-    path: '/bruckarte/lineup-add/:slug',
-    name: 'lineup-add',
-    component : LineupAdd
+    path: "/bruckarte/admin-panel",
+    name: "admin-panel",
+    component: AdminPanel,
   },
   {
-    path: '/bruckarte/sponsors-add/:slug',
-    name: 'sponsors-add',
-    component : SponsorsAdd
-  },{
-    path: '/bruckarte/band-kontakt',
-    name: 'band-kontakt',
-    component : BandKontakt
-  },{
-    path: '/bruckarte/page-not-found',
-    name: 'PageNotFound',
-    component : PageNotFound
+    path: "/bruckarte/lineup-add/:slug",
+    name: "lineup-add",
+    component: LineupAdd,
   },
-  
+  {
+    path: "/bruckarte/sponsors-add/:slug",
+    name: "sponsors-add",
+    component: SponsorsAdd,
+  },
+  {
+    path: "/bruckarte/band-kontakt",
+    name: "band-kontakt",
+    component: BandKontakt,
+  },
+  {
+    path: "/bruckarte/page-not-found",
+    name: "PageNotFound",
+    component: PageNotFound,
+  },
+
   /* Brucweb Views setup */
   {
-    path: '/sponsors',
-    name: 'bwsponsors',
-    component : BWsponsors
+    path: "/sponsors",
+    name: "bwsponsors",
+    component: BWsponsors,
   },
   {
-    path: '/lineup',
-    name: 'bwlineup',
-    component : Bwlineup
+    path: "/lineup",
+    name: "bwlineup",
+    component: Bwlineup,
   },
   {
-    path: '/kontakt',
-    name: 'kontakt',
-    component : Kontakt
+    path: "/kontakt",
+    name: "kontakt",
+    component: Kontakt,
   },
   {
-    path: '/ulaznice',
-    name: 'ulaznice',
-    component : Ulaznice
+    path: "/ulaznice",
+    name: "ulaznice",
+    component: Ulaznice,
   },
   {
-    path: '/pravila-ponasanja',
-    name: 'pravila-ponasanja',
-    component : Pravila
+    path: "/pravila-ponasanja",
+    name: "pravila-ponasanja",
+    component: Pravila,
   },
   {
-    path: '/',
-    name: 'naslovnica',
-    component : Naslovnica
+    path: "/",
+    name: "naslovnica",
+    component: Naslovnica,
   },
-  { path: '/:pathMatch(.*)*', component: BWPageNotFound },
-
-
-]
-
-
+  { path: "/:pathMatch(.*)*", component: BWPageNotFound },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
 router.beforeEach((to, from, next) => {
-  
   //provjera auth i privilegija
-  var allowedRoutesForprivilege2= ["home","login","logout","guest_tag","",undefined,"bruckarte"]
-  var allowedRoutesForprivilege3= ["home","login","logout","guests","",undefined]
-  var allowedRoutesForprivilege4= ["home","login","logout","guest_tag","guests","",undefined]
+  var allowedRoutesForprivilege2 = [
+    "home",
+    "login",
+    "logout",
+    "guest_tag",
+    "",
+    undefined,
+    "bruckarte",
+  ];
+  var allowedRoutesForprivilege3 = [
+    "home",
+    "login",
+    "logout",
+    "guests",
+    "",
+    undefined,
+  ];
+  var allowedRoutesForprivilege4 = [
+    "home",
+    "login",
+    "logout",
+    "guest_tag",
+    "guests",
+    "",
+    undefined,
+  ];
 
-  
-  if((String(to.path).split("/"))[1]=="bruckarte"){
-    if((Date.now()/1000)>store.state.tokenExp && to.name!='logout'){
-      next({path: '/bruckarte/logout'});
-    }else{
-    if(store.state.id=='' && to.name!='login'){
-      next({path: '/bruckarte/login'});
-    }else if((store.state.privilege==0 && to.name!='login')){
-      window.alert("Nažalost, nemate privilegije za pristup ovoj stranici. Pričekajte ili se javite savjetniku");
-      next({path: '/bruckarte/login'});
-    }else if((store.state.privilege==2 && !allowedRoutesForprivilege2.includes(to.name))){
-        window.alert("Nažalost, nemate privilegije za pristup ovoj stranici. Pričekajte ili se javite savjetniku");
-        next({path: '/bruckarte/home'});
-    }else if((store.state.privilege==3 && !allowedRoutesForprivilege3.includes(to.name))){
-        window.alert("Nažalost, nemate privilegije za pristup ovoj stranici. Pričekajte ili se javite savjetniku");
-        next({path: '/bruckarte/home'});
-    }else if((store.state.privilege==4 && !allowedRoutesForprivilege4.includes(to.name))){
-      window.alert("Nažalost, nemate privilegije za pristup ovoj stranici. Pričekajte ili se javite savjetniku");
-      next({path: '/bruckarte/home'});
-    }else{
-      next();
-    }}
-  }else{
+  if (String(to.path).split("/")[1] == "bruckarte") {
+    if (Date.now() / 1000 > store.state.tokenExp && to.name != "logout") {
+      next({ path: "/bruckarte/logout" });
+    } else {
+      if (store.state.id == "" && to.name != "login") {
+        next({ path: "/bruckarte/login" });
+      } else if (store.state.privilege == 0 && to.name != "login") {
+        window.alert(
+          "Nažalost, nemate privilegije za pristup ovoj stranici. Pričekajte ili se javite savjetniku"
+        );
+        next({ path: "/bruckarte/login" });
+      } else if (
+        store.state.privilege == 2 &&
+        !allowedRoutesForprivilege2.includes(to.name)
+      ) {
+        window.alert(
+          "Nažalost, nemate privilegije za pristup ovoj stranici. Pričekajte ili se javite savjetniku"
+        );
+        next({ path: "/bruckarte/home" });
+      } else if (
+        store.state.privilege == 3 &&
+        !allowedRoutesForprivilege3.includes(to.name)
+      ) {
+        window.alert(
+          "Nažalost, nemate privilegije za pristup ovoj stranici. Pričekajte ili se javite savjetniku"
+        );
+        next({ path: "/bruckarte/home" });
+      } else if (
+        store.state.privilege == 4 &&
+        !allowedRoutesForprivilege4.includes(to.name)
+      ) {
+        window.alert(
+          "Nažalost, nemate privilegije za pristup ovoj stranici. Pričekajte ili se javite savjetniku"
+        );
+        next({ path: "/bruckarte/home" });
+      } else {
+        next();
+      }
+    }
+  } else {
     next();
   }
-  
-})
+});
 
-export default router
+export default router;
