98c113fc51783fb31c515852206f73963e454097
pavleerg <pavleergovic@gmail.com>
2022-09-26 20:56:04 +0200

    updated .env.examples

--- brucifer/settings.py
+++ brucifer/settings.py
@@ -108,8 +108,8 @@ DATABASES = {
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pavle.ergovic@kset.org' 
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
 # Password validation
