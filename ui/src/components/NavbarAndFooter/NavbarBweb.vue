893ee406bd99b7d43482a154bd4b4a73dad3b82d
pavleerg <pavleergovic@gmail.com>
2022-09-29 19:02:09 +0200

    removed sponsors for overlay when visibility is 0

--- ui/src/components/NavbarAndFooter/NavbarBweb.vue
+++ ui/src/components/NavbarAndFooter/NavbarBweb.vue
@@ -40,7 +40,7 @@
           <RouterElement class="overlay-element " :name="'Ulaznice'" :link="'/ulaznice'" @click="toggleNav()">
           </RouterElement>
 
-          <RouterElement class="overlay-element " :name="'Sponzori'" :link="'/sponsors'" @click="toggleNav()">
+          <RouterElement v-if="this.sponsorsVisible=='1'" class="overlay-element " :name="'Sponzori'" :link="'/sponsors'" @click="toggleNav()">
           </RouterElement>
 
           <RouterElement class="overlay-element " :name="'Kontakt'" :link="'/kontakt'" @click="toggleNav()">
