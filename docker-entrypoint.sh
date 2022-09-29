c39b3688bb5e390f2bbde4fdea970239329b69ff
pavleerg <pavleergovic@gmail.com>
2022-09-29 18:56:11 +0200

    changed docker - entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,5 @@
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata init_data.json
 python initadmin.py
 python manage.py runserver 0.0.0.0:8000
