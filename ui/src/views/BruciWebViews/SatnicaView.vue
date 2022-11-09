acd75d1e46b5fa3811e74a3ebb7ba2abe7fe8bc7
pavleerg <pavleergovic@gmail.com>
2022-11-09 13:58:21 +0100

    fixed images in satnica and tlocrt

--- ui/src/views/BruciWebViews/SatnicaView.vue
+++ ui/src/views/BruciWebViews/SatnicaView.vue
@@ -1,74 +1,97 @@
 <template>
-    <div class="page-container">
+  <div class="page-container">
     <NavbarBweb></NavbarBweb>
-      <div class="satnica">
-          <div class="image">
-            <img class="satnica-desktop" src="../../assets/satnica/satnica-desktop.svg" alt="Brucifer Tlocrt"/>
-            <img class="satnica-tablet" src="../../assets/satnica/satnica-tablet.svg" alt="Brucifer Tlocrt" />
-          </div>
-  
+    <div class="satnica">
+      <div class="image">
+        <img class="satnica-desktop" src="../../assets/satnica/satnica-desktop.svg" alt="Brucifer Satnica" />
+        <img class="satnica-tablet" src="../../assets/satnica/satnica-tablet.svg" alt="Brucifer Satnica" />
+        <img class="satnica-mobile" src="../../assets/satnica/satnica-mobile.svg" alt="Brucifer Satnica" />
       </div>
-      <Footer></Footer>
-  
-    </div>
-  </template>
-  
-  <script>
-  import Footer from '@/components/NavbarAndFooter/Footer.vue'
-  import NavbarBweb from '@/components/NavbarAndFooter/NavbarBweb.vue'
 
-  export default {
-    name: 'Naslovnica',
-    components: { Footer, NavbarBweb },
-    props: {
-      msg: String
-    },
-    }
-  </script>
+    </div>
+    <Footer></Footer>
+
+  </div>
+</template>
   
-  <style scoped>
-  .satnica {
-    width: 100%;
-    min-height: 91vh;
-    padding-top: 3rem;
+<script>
+import Footer from '@/components/NavbarAndFooter/Footer.vue'
+import NavbarBweb from '@/components/NavbarAndFooter/NavbarBweb.vue'
+
+export default {
+  name: 'Naslovnica',
+  components: { Footer, NavbarBweb },
+  props: {
+    msg: String
+  },
+}
+</script>
+  
+<style scoped>
+.satnica {
+  width: 100%;
+  min-height: 91vh;
+  padding-top: 3rem;
+}
+
+.satnica-desktop {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+
+.satnica-tablet {
+  display: none !important;
+}
+
+.satnica-mobile {
+  display: none !important;
+}
+
+.image {
+  width: 100%;
+  height: 100%;
+}
+
+.navbar.bw {
+  background: #c15476;
+}
+
+.navbar.bw::after {
+  background: linear-gradient(#c15476, transparent);
+}
+
+@media screen and (max-width: 980px) {
+  .satnica-desktop {
+    display: none !important;
   }
 
-  .satnica-desktop {
-    width: 100%;
+  .satnica-tablet {
     height: 100%;
-    display: block;
+    display: block !important;
+  }
+
+  .satnica-mobile {
+    display: none !important;
+  }
+}
+
+@media screen and (max-width: 550px) {
+  .satnica-desktop {
+    display: none !important;
   }
 
   .satnica-tablet {
     display: none !important;
   }
 
-  .image {
-    width: 100%;
+  .satnica-mobile {
     height: 100%;
+    display: block !important;
   }
-
-  .navbar.bw {
-    background: #c15476;
-  }
-
-  .navbar.bw::after {
-    background: linear-gradient(#c15476, transparent);
-  }
-
-  @media screen and (max-width: 980px) {
-    .satnica-desktop {
-      display: none !important;
-    }
-
-    .satnica-tablet {
-      height: 100%;
-      display: block !important;
-    }
 
 }
-  
 
-  @import url(../../bruciweb.css);
-  </style>
+@import url(../../bruciweb.css);
+</style>
   
--- ui/src/views/BruciWebViews/TlocrtView.vue
+++ ui/src/views/BruciWebViews/TlocrtView.vue
@@ -1,74 +1,97 @@
 <template>
-    <div class="page-container">
+  <div class="page-container">
     <NavbarBweb></NavbarBweb>
-      <div class="tlocrt">
-          <div class="image">
-            <img class="tlocrt-desktop" src="../../assets/tlocrt/tlocrt-desktop.svg" alt="Brucifer Tlocrt"/>
-            <img class="tlocrt-tablet" src="../../assets/tlocrt/tlocrt-tablet.svg" alt="Brucifer Tlocrt" />
-          </div>
-  
+    <div class="tlocrt">
+      <div class="image">
+        <img class="tlocrt-desktop" src="../../assets/tlocrt/tlocrt-desktop.svg" alt="Brucifer Tlocrt" />
+        <img class="tlocrt-tablet" src="../../assets/tlocrt/tlocrt-tablet.svg" alt="Brucifer Tlocrt" />
+        <img class="tlocrt-mobile" src="../../assets/tlocrt/tlocrt-mobile.svg" alt="Brucifer Tlocrt" />
       </div>
-      <Footer></Footer>
-  
-    </div>
-  </template>
-  
-  <script>
-  import Footer from '@/components/NavbarAndFooter/Footer.vue'
-  import NavbarBweb from '@/components/NavbarAndFooter/NavbarBweb.vue'
 
-  export default {
-    name: 'Naslovnica',
-    components: { Footer, NavbarBweb },
-    props: {
-      msg: String
-    },
-    }
-  </script>
+    </div>
+    <Footer></Footer>
+
+  </div>
+</template>
   
-  <style scoped>
-  .tlocrt {
-    width: 100%;
-    min-height: 91vh;
-    padding-top: 3rem;
+<script>
+import Footer from '@/components/NavbarAndFooter/Footer.vue'
+import NavbarBweb from '@/components/NavbarAndFooter/NavbarBweb.vue'
+
+export default {
+  name: 'Naslovnica',
+  components: { Footer, NavbarBweb },
+  props: {
+    msg: String
+  },
+}
+</script>
+  
+<style scoped>
+.tlocrt {
+  width: 100%;
+  min-height: 91vh;
+  padding-top: 3rem;
+}
+
+.tlocrt-desktop {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+
+.tlocrt-tablet {
+  display: none !important;
+}
+
+.tlocrt-mobile {
+  display: none !important;
+}
+
+.image {
+  width: 100%;
+  height: 100%;
+}
+
+.navbar.bw {
+  background: #c15476;
+}
+
+.navbar.bw::after {
+  background: linear-gradient(#c15476, transparent);
+}
+
+@media screen and (max-width: 980px) {
+  .tlocrt-desktop {
+    display: none !important;
   }
 
-  .tlocrt-desktop {
-    width: 100%;
+  .tlocrt-tablet {
     height: 100%;
-    display: block;
+    display: block !important;
+  }
+
+  .tlocrt-mobile {
+    display: none !important;
+  }
+}
+
+@media screen and (max-width: 550px) {
+  .tlocrt-desktop {
+    display: none !important;
   }
 
   .tlocrt-tablet {
     display: none !important;
   }
 
-  .image {
-    width: 100%;
+  .tlocrt-mobile {
     height: 100%;
+    display: block !important;
   }
-
-  .navbar.bw {
-    background: #c15476;
-  }
-
-  .navbar.bw::after {
-    background: linear-gradient(#c15476, transparent);
-  }
-
-  @media screen and (max-width: 980px) {
-    .tlocrt-desktop {
-      display: none !important;
-    }
-
-    .tlocrt-tablet {
-      height: 100%;
-      display: block !important;
-    }
 
 }
-  
 
-  @import url(../../bruciweb.css);
-  </style>
+@import url(../../bruciweb.css);
+</style>
   
