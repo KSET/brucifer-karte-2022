b8bf3a3f30e8b2d5c52d3f61c256e1aee8366e47
pavleerg <pavleergovic@gmail.com>
2023-04-13 13:40:42 +0200

    update color of navbar elements

--- ui/src/components/NavbarAndFooter/NavbarAdmin.vue
+++ ui/src/components/NavbarAndFooter/NavbarAdmin.vue
@@ -1,99 +1,229 @@
 <template>
   <div class="navbar admin">
-    <router-link style="left:  1.7rem !important;" class="navbar-title" :class="$style.hideOnSmall" to="/admin/">
+    <router-link
+      style="left: 1.7rem !important"
+      class="navbar-title"
+      :class="$style.hideOnSmall"
+      to="/admin/"
+    >
       #BRUCIFER
     </router-link>
 
     <div class="routes">
+      <RouterElement
+        v-if="privilege == '1' || privilege == '3' || privilege == '4'"
+        class="navbar-element hidemobile"
+        :name="'Brucoši'"
+        :link="'/admin/guests'"
+      ></RouterElement>
 
-      <RouterElement v-if="privilege == '1' || privilege == '3' || privilege == '4'" class="navbar-element hidemobile"
-        :name="'Brucoši'" :link="'/admin/guests'"></RouterElement>
+      <RouterElement
+        v-if="privilege == '1' || privilege == '2' || privilege == '4'"
+        class="navbar-element hidemobile"
+        :name="'Ulaz'"
+        :link="'/admin/entry'"
+      ></RouterElement>
 
-      <RouterElement v-if="privilege == '1' || privilege == '2' || privilege == '4'" class="navbar-element hidemobile"
-        :name="'Ulaz'" :link="'/admin/entry'"></RouterElement>
-
-      <router-link v-if="privilege == '1'" class="navbar-element hidetablet" to="/admin/admin-panel">
-        <img src="../../assets/icons/nav-burger.svg">
+      <router-link
+        v-if="privilege == '1'"
+        class="navbar-element hidetablet"
+        to="/admin/admin-panel"
+      >
+        <img src="../../assets/icons/nav-burger.svg" />
       </router-link>
-      <div id="nav-icon3" style="  margin-left: 70px;" v-if="privilege != ''" class="navbar-element hidedesktop"
-        @click="toggleNav">
+      <div
+        id="nav-icon3"
+        style="margin-left: 70px"
+        v-if="privilege != ''"
+        class="navbar-element hidedesktop"
+        @click="toggleNav"
+      >
         <span></span>
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <router-link v-if="privilege != '' && privilege != '0'" class="navbar-element lg" to="/admin/logout">
-        <img src="../../assets/icons/logout-icon.svg">
+      <router-link
+        v-if="privilege != '' && privilege != '0'"
+        class="navbar-element lg"
+        to="/admin/logout"
+      >
+        <img src="../../assets/icons/logout-icon.svg" />
       </router-link>
-
-
 
       <div id="myNav" class="overlay admin">
         <div class="overlay-content admin">
-          <RouterElement class="overlay-element admin hidetablet showmobile"
-            v-if="privilege == '1' || privilege == '3' || privilege == '4'" @click="toggleNav()" :name="'Brucoši'"
-            :link="'/admin/guests'">
+          <RouterElement
+            class="overlay-element admin hidetablet showmobile"
+            v-if="privilege == '1' || privilege == '3' || privilege == '4'"
+            @click="toggleNav()"
+            :name="'Brucoši'"
+            :link="'/admin/guests'"
+          >
           </RouterElement>
-          <RouterElement class="overlay-element admin hidetablet showmobile"
-            v-if="privilege == '1' || privilege == '2' || privilege == '4'" @click="toggleNav()" :name="'Ulaz'"
-            :link="'/admin/entry'">
+          <RouterElement
+            class="overlay-element admin hidetablet showmobile"
+            v-if="privilege == '1' || privilege == '2' || privilege == '4'"
+            @click="toggleNav()"
+            :name="'Ulaz'"
+            :link="'/admin/entry'"
+          >
           </RouterElement>
-          <RouterElement class="overlay-element admin" v-if="privilege == '1'" @click="toggleNav()" :name="'Tagovi'"
-            :link="'/admin/tags'">
+          <RouterElement
+            class="overlay-element admin"
+            v-if="privilege == '1'"
+            @click="toggleNav()"
+            :name="'Tagovi'"
+            :link="'/admin/tags'"
+          >
           </RouterElement>
-          <RouterElement class="overlay-element admin" v-if="privilege == '1'" @click="toggleNav()"
-            :name="'Privilegije'" :link="'/admin/privileges'"></RouterElement>
-          <RouterElement class="overlay-element admin" v-if="privilege == '1'" @click="toggleNav()" :name="'Korisnici'"
-            :link="'/admin/users'">
+          <RouterElement
+            class="overlay-element admin"
+            v-if="privilege == '1'"
+            @click="toggleNav()"
+            :name="'Privilegije'"
+            :link="'/admin/privileges'"
+          ></RouterElement>
+          <RouterElement
+            class="overlay-element admin"
+            v-if="privilege == '1'"
+            @click="toggleNav()"
+            :name="'Korisnici'"
+            :link="'/admin/users'"
+          >
           </RouterElement>
-          <RouterElement class="overlay-element admin" v-if="privilege == '1'" @click="toggleNav()" :name="'Uvoz'"
-            :link="'/admin/import'">
+          <RouterElement
+            class="overlay-element admin"
+            v-if="privilege == '1'"
+            @click="toggleNav()"
+            :name="'Uvoz'"
+            :link="'/admin/import'"
+          >
           </RouterElement>
 
-          <button class="overlay-element admin" v-if="privilege == '1'" @click="ExportData()">
+          <button
+            class="overlay-element admin"
+            v-if="privilege == '1'"
+            @click="ExportData()"
+          >
             Izvoz
           </button>
 
-          <div class="sidbar-element" v-if="privilege == '1'" @click="toggleDropdownLineup">
-            <RouterElement class="overlay-element admin" style="left: 0%; position: abosolute; display: inline-block;"
-              :name="'Izvođači'">
+          <div
+            class="sidbar-element"
+            v-if="privilege == '1'"
+            @click="toggleDropdownLineup"
+          >
+            <RouterElement
+              class="overlay-element admin"
+              style="left: 0%; position: abosolute; display: inline-block"
+              :name="'Izvođači'"
+            >
             </RouterElement>
-            <img v-if="this.showDropdownLineup == false" class="dropdown-icon"
-              src="@/assets/icons/dopdwn-notopen-icon.svg" @click="toggleDropdownLineup">
-            <img v-else class="dropdown-icon" src="@/assets/icons/dopdwn-open-icon.svg" @click="toggleDropdownLineup">
-
+            <img
+              v-if="this.showDropdownLineup == false"
+              class="dropdown-icon"
+              src="@/assets/icons/dopdwn-notopen-icon.svg"
+              @click="toggleDropdownLineup"
+            />
+            <img
+              v-else
+              class="dropdown-icon"
+              src="@/assets/icons/dopdwn-open-icon.svg"
+              @click="toggleDropdownLineup"
+            />
           </div>
-          <RouterElement id="dpL11" class="overlay-element admin" @click="toggleNav()" :name="'Pregled Izvođača'"
-            style="margin-left: 30px;" :link="'/admin/lineup-list'">
+          <RouterElement
+            id="dpL11"
+            class="overlay-element admin"
+            @click="toggleNav()"
+            :name="'Pregled Izvođača'"
+            style="margin-left: 30px"
+            :link="'/admin/lineup-list'"
+          >
           </RouterElement>
-          <RouterElement id="dpL21" class="overlay-element admin" @click="toggleNav()" :name="'Dodavanje Izvođača'"
-            style="margin-left: 30px;" :link="'/admin/lineup-add/0'">
+          <RouterElement
+            id="dpL21"
+            class="overlay-element admin"
+            @click="toggleNav()"
+            :name="'Dodavanje Izvođača'"
+            style="margin-left: 30px"
+            :link="'/admin/lineup-add/0'"
+          >
           </RouterElement>
 
-          <div class="sidbar-element" v-if="privilege == '1'" @click="toggleDropdownSponsors">
-            <RouterElement class="overlay-element admin" style="left: 0%; position: abosolute; display: inline-block;"
-              :name="'Sponzori'">
+          <div
+            class="sidbar-element"
+            v-if="privilege == '1'"
+            @click="toggleDropdownSponsors"
+          >
+            <RouterElement
+              class="overlay-element admin"
+              style="left: 0%; position: abosolute; display: inline-block"
+              :name="'Sponzori'"
+            >
             </RouterElement>
-            <img v-if="this.showDropdownSponsors == false" class="dropdown-icon" style="display: inline-block"
-              src="@/assets/icons/dopdwn-notopen-icon.svg" @click="toggleDropdownSponsors">
-            <img v-else class="dropdown-icon" src="@/assets/icons/dopdwn-open-icon.svg" @click="toggleDropdownSponsors">
-
+            <img
+              v-if="this.showDropdownSponsors == false"
+              class="dropdown-icon"
+              style="display: inline-block"
+              src="@/assets/icons/dopdwn-notopen-icon.svg"
+              @click="toggleDropdownSponsors"
+            />
+            <img
+              v-else
+              class="dropdown-icon"
+              src="@/assets/icons/dopdwn-open-icon.svg"
+              @click="toggleDropdownSponsors"
+            />
           </div>
-          <RouterElement id="dpS11" class="overlay-element admin" @click="toggleNav()" :name="'Pregled Sponzora'"
-            style="margin-left: 30px;" :link="'/admin/sponsors-list'">
+          <RouterElement
+            id="dpS11"
+            class="overlay-element admin"
+            @click="toggleNav()"
+            :name="'Pregled Sponzora'"
+            style="margin-left: 30px"
+            :link="'/admin/sponsors-list'"
+          >
           </RouterElement>
-          <RouterElement id="dpS21" class="overlay-element admin" @click="toggleNav()" :name="'Dodavanje Sponzora'"
-            style="margin-left: 30px;" :link="'/admin/sponsors-add/0'">
+          <RouterElement
+            id="dpS21"
+            class="overlay-element admin"
+            @click="toggleNav()"
+            :name="'Dodavanje Sponzora'"
+            style="margin-left: 30px"
+            :link="'/admin/sponsors-add/0'"
+          >
           </RouterElement>
 
-          <RouterElement class="overlay-element admin" v-if="privilege == '1'" :name="'Dodaj Gosta'"
-            @click="toggleNav()" :link="'/admin/guests-add'"></RouterElement>
-          <RouterElement class="overlay-element admin" v-if="privilege == '1'" :name="'Kontakt'" @click="toggleNav()"
-            :link="'/admin/band-kontakt'"></RouterElement>
-          <RouterElement class="overlay-element admin" v-if="privilege == '1'" :name="'Firme'" @click="toggleNav()"
-            :link="'/admin/firme'"></RouterElement>
-          <RouterElement class="overlay-element admin" v-if="privilege == '1'" :name="'Cjenik'" @click="toggleNav()"
-            :link="'/admin/cjenik'"></RouterElement>
+          <RouterElement
+            class="overlay-element admin"
+            v-if="privilege == '1'"
+            :name="'Dodaj Gosta'"
+            @click="toggleNav()"
+            :link="'/admin/guests-add'"
+          ></RouterElement>
+          <RouterElement
+            class="overlay-element admin"
+            v-if="privilege == '1'"
+            :name="'Kontakt'"
+            @click="toggleNav()"
+            :link="'/admin/band-kontakt'"
+          ></RouterElement>
+          <RouterElement
+            class="overlay-element admin"
+            v-if="privilege == '1'"
+            :name="'Firme'"
+            @click="toggleNav()"
+            :link="'/admin/firme'"
+          ></RouterElement>
+          <RouterElement
+            class="overlay-element admin"
+            v-if="privilege == '1'"
+            :name="'Cjenik'"
+            @click="toggleNav()"
+            :link="'/admin/cjenik'"
+          ></RouterElement>
         </div>
       </div>
     </div>
@@ -101,23 +231,23 @@
 </template>
 
 <script>
-import store from '@/store/index.js';
-import RouterElement from '@/components/AdminPanel/RouterElement.vue'
-import * as XLSX from 'xlsx'
-import axios from 'axios'
+import store from "@/store/index.js";
+import RouterElement from "@/components/AdminPanel/RouterElement.vue";
+import * as XLSX from "xlsx";
+import axios from "axios";
 
 export default {
-  name: 'Navbar',
-  el: '#app',
+  name: "Navbar",
+  el: "#app",
   components: {
-    RouterElement
+    RouterElement,
   },
   data() {
     return {
-      route: '',
+      route: "",
       showDropdownLineup: false,
       showDropdownSponsors: false,
-    }
+    };
   },
   computed: {
     privilege() {
@@ -125,7 +255,7 @@ export default {
     },
     name() {
       return store.state.name;
-    }
+    },
   },
   mounted() {
     this.route = this.$route.path;
@@ -136,18 +266,17 @@ export default {
   },
   methods: {
     ExportData() {
-      var filename = 'export_guests.xlsx';
-      axios.get(process.env.VUE_APP_BASE_URL + '/guests/',)
-        .then(response => {
-          var data = response.data;
-          var ws = XLSX.utils.json_to_sheet(data);
-          var wb = XLSX.utils.book_new();
-          XLSX.utils.book_append_sheet(wb, ws, "People");
-          XLSX.writeFile(wb, filename);
-        });
+      var filename = "export_guests.xlsx";
+      axios.get(process.env.VUE_APP_BASE_URL + "/guests/").then((response) => {
+        var data = response.data;
+        var ws = XLSX.utils.json_to_sheet(data);
+        var wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, "People");
+        XLSX.writeFile(wb, filename);
+      });
     },
     toggleNav() {
-      document.getElementById("nav-icon3").classList.toggle('open');
+      document.getElementById("nav-icon3").classList.toggle("open");
       if (this.showNav) {
         document.getElementById("myNav").style.height = "0%";
       } else {
@@ -175,9 +304,8 @@ export default {
         document.getElementById("dpS21").style.display = "none";
       }
     },
-  }
-}
-
+  },
+};
 </script>
 <style module lang="scss">
 .hideOnSmall {
@@ -200,20 +328,18 @@ export default {
   width: 100%;
   /* Full width */
   height: 3.75rem;
-
-
 }
 
 .navbar.admin {
-  background: #FFFFFF;
+  background: #ffffff;
   border-bottom: 1px solid #000000;
 
-  font-family: 'Montserrat';
+  font-family: "Montserrat";
   font-style: normal;
 }
 
 .navbar.bw {
-  font-family: 'Antonio';
+  font-family: "Antonio";
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
@@ -244,7 +370,6 @@ export default {
   top: 20%;
   bottom: 20%;
 
-
   font-weight: 700;
   font-size: 32px;
   line-height: 36px;
@@ -268,7 +393,7 @@ export default {
   font-family: inherit;
   margin-left: 70px;
   vertical-align: middle;
-
+  color: black;
   border: none;
   background: none;
   text-align: left;
@@ -290,7 +415,7 @@ export default {
   padding-bottom: 3%;
   padding-right: 5%;
 
-  font-family: 'Montserrat';
+  font-family: "Montserrat";
   font-style: normal;
   font-weight: 700;
   font-size: 32px;
@@ -303,12 +428,11 @@ export default {
 }
 
 .textfield {
-  font-family: 'Montserrat';
+  font-family: "Montserrat";
   font-style: normal;
   font-weight: 700;
   font-size: 16px;
   vertical-align: middle;
-
 }
 
 .showmobile {
@@ -331,7 +455,6 @@ export default {
   .showmobile {
     display: none !important;
   }
-
 }
 
 @media screen and (max-width: 550px) {
@@ -363,14 +486,11 @@ export default {
     margin-bottom: 7px;
 
     padding-left: 40px;
-    padding-bottom: 10px,
+    padding-bottom: 10px;
   }
 
   .navbar-element.lg {
     margin-left: 10px;
-
   }
 }
 </style>
-
-
