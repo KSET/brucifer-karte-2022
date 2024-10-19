3185fb60afb20598d4008ad6be5fba62ccccd46a
pavleerg <pavleergovic@gmail.com>
2024-10-20 00:41:44 +0200

    added staticfiles

--- brucifer/settings.py
+++ brucifer/settings.py
@@ -155,6 +155,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
