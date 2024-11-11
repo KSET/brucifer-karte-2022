f112eee23e849be9f7058c1badef5f3aee7d1e26
pavleerg <pavle.ergovic@fer.hr>
2024-11-11 23:38:58 +0100

    changes

--- brucifer/settings.py
+++ brucifer/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
